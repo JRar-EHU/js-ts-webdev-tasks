88cec5a447d68105fd4fc5f8d03cf8fcd449309a
Dmitriy Yartsev <iartsev.dmitrii@student.ehu.lt>
2025-04-09 17:44:46 +0300

    fix: optimized with bulma

--- main2.js
+++ main2.js
@@ -13,131 +13,84 @@ const NAV_LIST =[
 ];
 const DATA = [
     {
-        preview: "./assets/slub jersey t-shirt.png",
-        name:"Slub jersey T-shirt",
+        preview: "slub jersey t-shirt.png",
+        name: "Slub jersey T-shirt",
     },
     {
-        preview: "./assets/printed t-shirt.png",
-        name:"Printed T-shirt",
+        preview: "printed t-shirt.png",
+        name: "Printed T-shirt",
     },
     {
-        preview: "./assets/Cotton T-shirt.png",
-        name:"Cotton T-shirt",
+        preview: "cotton T-shirt.png",
+        name: "Cotton T-shirt",
     },
     {
-        preview: "./assets/t-shirt with a motif.png",
-        name:"T-shirt with a motif",
+        preview: "t-shirt with a motif.png",
+        name: "T-shirt with a motif",
     },
     {
-        preview: "./assets/cotton t-shirt regular fit.png",
-        name:"Cotton T-shirt regular fit",
+        preview: "cotton t-shirt regular fit.png",
+        name: "Cotton T-shirt regular fit",
     },
     {
-        preview: "./assets/slub jersey t-shirt 2.png",
-        name:"Slub jersey T-shirt",
+        preview: "slub jersey t-shirt 2.png",
+        name: "Slub jersey T-shirt",
     },
 
 ];
 
-const main = document.getElementsByTagName("main")[0];
+    // Nav Section
 
-// Nav section
-
-// const navFragment = document.createDocumentFragment();
-// NAV_LIST.forEach((item) => {
-//     const navElement = document.createElement("li");
-//     navElement.innerText = item;
-//     navFragment.append(navElement);
-// });
-// const ul = document.createElement("ul");
-// ul.append(navFragment);
-//
-// const navContainer = document.createElement("section");
-// navContainer.append(ul);
-// main.append(navContainer);
-const navFragment = document.createDocumentFragment();
+const liFragment = document.createDocumentFragment();
 NAV_LIST.forEach((item) => {
-    const navElementContainer = document.createElement("div");
-    navElementContainer.innerHTML = `
-        <button>${item}</button>
-    `;
-    navFragment.append(navElementContainer);
-})
-
-const navContainer = document.createElement("div");
-navContainer.append(navFragment);
-
-const mainNavContainer = document.createElement("section");
-mainNavContainer.append(navContainer);
-
-main.append(mainNavContainer);
-
-// Items section
-
-const mainProductsContainer = document.createElement("section");
-
-        // Head
-
-const headContainer = document.createElement("div");
-headContainer.innerHTML = `
-    <div>6 items</div>
-    <div>
-        <span>sort by</span>
-        <span>recommended</span>
-    </div>
-`;
-mainProductsContainer.append(headContainer);
-
-        // Items
-
-const itemsContainer = document.createElement("div");
-
-const itemFragment = document.createDocumentFragment();
-DATA.forEach(item => {
-    const elementContainer = document.createElement("div");
-    elementContainer.innerHTML = `
-            <img 
-            src="${item.preview}"
-            alt="preview"
-        >
-            <p>${item.name}</p>
-            <p>$ 12.99</p>
-            <button>Add to bag</button>
-    `
-    itemFragment.append(elementContainer);
+   const li = document.createElement("li");
+   li.classList.add("m-1","liButton");
+   li.innerHTML = `
+        <button class="text" data-category="asideUlButtons">${item}</button>
+   `
+    liFragment.append(li);
 });
-// DATA.forEach(item => {
-//     const elementContainer = document.createElement("div");
-//     const preview = document.createElement("img");
-//     preview.setAttribute("src", item.preview);
-//     const name = document.createElement("p");
-//     name.textContent = item.name;
-//     const price = document.createElement("p");
-//     price.innerText = "$ 12.99";
-//     const button = document.createElement("button");
-//     button.textContent = "Add to bag";
-//
-//     elementContainer.append(preview,name,price,button);
-//     itemFragment.append(elementContainer);
-// });
-itemsContainer.append(itemFragment);
 
-const boxItemContainer = document.createElement("div");
-
-boxItemContainer.append(itemsContainer);
-mainProductsContainer.append(boxItemContainer);
-main.append(mainProductsContainer);
+const ul = document.getElementById("asideUlList");
+ul.append(liFragment)
 
 
-// const eventArea = document.querySelector(".navContainer");
-// eventArea.addEventListener("click", (e) => {
-//     const button = e.target.closest("button");
-//     if(!button) {
-//         return;
-//     }
-//     console.log(e.target);
-//     const buttons = document.querySelectorAll(".navButton");
-//     buttons.forEach(btn => btn.classList.remove("is-active"));
-//     button.classList.add("is-active");
-// })
+    // Items Section
+
+const cardsFragment = document.createDocumentFragment();
+DATA.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("column","is-one-third");
+    card.innerHTML = `
+            <div class="card is-transparent">
+                <div class="card-image">
+                    <figure class="image">
+                        <img src="./assets/${item.preview}" alt="preview" >
+                    </figure>
+                </div>
+                <div class="content is-flex is-flex-direction-column is-align-items-center">
+                    <p class="cardText m-1" >${item.name}</p>
+                    <p class="card-price m-1">$ 12.99</p>
+                    <button class="button is-rounded m-1">Add to bag</button>
+                </div>
+            </div>
+    `
+    cardsFragment.append(card);
+});
+const cardsContainer = document.getElementById("cardsContainer");
+cardsContainer.append(cardsFragment);
+
+    // Events
+
+ul.addEventListener("click", (e) => {
+    const button = e.target.closest("button");
+    if(!button) {
+        return;
+    }
+    console.log(e.target);
+
+    const buttons = document.querySelectorAll('[data-category="asideUlButtons"]');
+    buttons.forEach(btn => btn.classList.remove("is-active"));
+    button.classList.add("is-active");
+})
 
